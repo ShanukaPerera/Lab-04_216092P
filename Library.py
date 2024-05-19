e23342f2d0ec11b88f5a9cef629b662103c5dfb9
Shanuka <shanukadilharaperera@gmail.com>
2024-05-19 10:36:35 +0530

    modify 1

--- Library.py
+++ Library.py
@@ -1,3 +1,11 @@
 print("Hello World")
 book = "Oliver Twist"
 print(book)
+
+from book import book
+book_1 = book ("A" , "A B Perera", 1000)
+book_2 = book ("B" , "C D Silva", 2000)
+
+print(book_1.name)
+print(book_2.price)x
+
